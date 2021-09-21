62e6bd52b98526d38266d60cb236ee55b6fa2bd2
Mark Rucker <rucker.mark@gmail.com>
2021-09-20 20:37:39 -0400

    Removing all FOREX datasets.

--- study1/test.py
+++ study1/test.py
@@ -21,7 +21,7 @@ d            = 4
 c            = 25
 megalr       = 0.1
 
-processes  = 1
+processes  = 6
 shuffle    = [1]
 take       = 2000
 
@@ -43,11 +43,15 @@ scorer4 = ClassScorer(base="none", exampler=DiffSquareExampler(), interactions=[
 #scorer3 = RegrScorer (base="cos", exampler=DiffSquareExampler(), interactions=[], ignored=[])
 #scorer4 = ClassScorer(base="l2" , exampler=DiffSquareExampler(), interactions=[], ignored=[])
 
+#[x1,x2,x3] [y1,y2,y3]
+#[(x1-y1)**2, (x2-y2)**2, (x3-y3)**2] = ||x-y||^2
+
+
 #TEST BED
 #scorer1 = RegrScorer(base="none",exampler=DiffSquareExampler(), interactions=[], ignored=[])
 #scorer2 = RegrScorer(base="mem" ,exampler=DiffSquareExampler(), interactions=[], ignored=[])
 #scorer3 = RegrScorer(base="l2"  ,exampler=DiffSquareExampler(), interactions=[], ignored=[])
-scorer4 = ClassScorer(base="cos" ,exampler=DiffSquareExampler(), interactions=[], ignored=[])
+#scorer4 = ClassScorer(base="cos" ,exampler=DiffSquareExampler(), interactions=[], ignored=[])
 
 cmt_1 = CMT_Implemented(max_memories, scorer=scorer1, signal=RwdFeedback(), c=c, d=d, megalr=megalr)
 cmt_2 = CMT_Implemented(max_memories, scorer=scorer2, signal=RwdFeedback(), c=c, d=1, megalr=megalr)
@@ -55,9 +59,9 @@ cmt_3 = CMT_Implemented(max_memories, scorer=scorer3, signal=RwdFeedback(), c=c,
 cmt_4 = CMT_Implemented(max_memories, scorer=scorer4, signal=RwdFeedback(), c=c, d=1, megalr=megalr)
 
 learners = [
-   #MemorizedLearner(epsilon, cmt_1),
-   #MemorizedLearner(epsilon, cmt_2),
-   #MemorizedLearner(epsilon, cmt_3),
+   MemorizedLearner(epsilon, cmt_1),
+   MemorizedLearner(epsilon, cmt_2),
+   MemorizedLearner(epsilon, cmt_3),
    MemorizedLearner(epsilon, cmt_4),
 ]
 
