2024a3bb400af3199750e56bbb1246466afbe9f0
Mark Rucker <rucker.mark@gmail.com>
2021-10-17 15:32:14 -0400

    Updated run with power_t and no power_t

--- study1/run.py
+++ study1/run.py
@@ -12,10 +12,12 @@ from learners import MemorizedLearner, MemCorralLearner, CMT_Implemented
 from scorers import RankScorer, RegressionScorer, UCBScorer, Base
 from examples import InteractionExample, DifferenceExample
 
-from coba.benchmarks import Benchmark
+from coba.benchmarks import Benchmark, Result
 from coba.learners import VowpalLearner
 
-experiment = 'full4'
+Result.from_file("./study1/outcomes/full4_3.log.gz").filter_fin().interactions.to_tuples()
+
+experiment = 'test'
 processes  = 7
 chunk_by   = 'task'
 
@@ -26,7 +28,7 @@ c            = 40
 megalr       = 0.1
 
 json = f"./study1/experiments/{experiment}.json"
-log  = f"./study1/outcomes/{experiment}_1.log.gz"
+log  = f"./study1/outcomes/{experiment}_2.log.gz"
 
 scorers = [
    RankScorer(baser=Base("cos") , exampler=DifferenceExample("abs")),
@@ -36,13 +38,14 @@ feedbacks = [
    DeviationFeedback("^2"),
 ]
 
-vw_cb = VowpalLearner(epsilon=epsilon,seed=1,power_t=0)
+vw_cb_1 = VowpalLearner("--cb_explore_adf --interactions ssa --interactions sa --ignore_linear s --epsilon 0.1 --random_seed 1 --power_t 0")
+vw_cb_2 = VowpalLearner("--cb_explore_adf --interactions ssa --interactions sa --ignore_linear s --epsilon 0.1 --random_seed 1")
 
 cmts     = [ CMT_Implemented(max_memories, scorer=s, feedback=f, c=c, d=d, megalr=megalr) for s,f in zip(scorers,feedbacks)]
 mem_cbs  = [ MemorizedLearner(epsilon, cmt) for cmt in cmts]
-learners = [ MemCorralLearner([vw_cb, mem_cb], eta=.075, T=10000, type="off-policy") for mem_cb in mem_cbs ]
+learners = [ MemCorralLearner([vw_cb_1, mem_cb], eta=.075, T=10000, type="off-policy") for mem_cb in mem_cbs ]
 
-learners += [ vw_cb ]
+learners = [ vw_cb_1, vw_cb_2 ]
 
 if __name__ == '__main__':
    Benchmark.from_file(json).processes(processes).chunk_by(chunk_by).evaluate(learners, log).filter_fin().plot_learners()
